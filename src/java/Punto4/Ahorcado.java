08e549208690ce195e1f4401237d963f655999ea
dhal_ <dhal_@LAPTOP-J53S6BN7>
2018-05-06 21:55:14 -0300

    agregar de paquete punto4 y su clase ahorcado

---
+++ src/java/Punto4/Ahorcado.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto4;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author dhal_
+ */
+public class Ahorcado {
+
+    private ArrayList<Character> letrasUsada;
+    private String[] palabras = {"hola", "casa", "agentina", "escuela", "internet", "programacion", "dormitorio", "informatica", "ingenieria"};
+    private String palabra;
+    private String palabraElegida;
+    private String resultado;
+    private char letra;
+    private boolean coinciden;
+    private boolean estado;
+    private int intento;
+    private int fallo;
+    private int cont;
+
+    private Random rand = new Random(System.nanoTime());
+
+    public Ahorcado() {
+        this.palabra = palabras[rand.nextInt(palabras.length)];
+        this.palabraElegida = inicializarPalabra();
+        this.letrasUsada = new ArrayList<>();
+         this.intento = palabra.length();
+        this.fallo = 0;
+        this.cont = 0;
+        
+       
+    }
+   
+
+    public String inicializarPalabra() {
+        palabraElegida = "";
+        for (int i = 0; i < getPalabra().length(); i++) {
+            palabraElegida = palabraElegida + "*";
+        }
+        return palabraElegida;
+    }
+
+    //Metodo para comparar letra introducida con una palabra del array de palabras
+    public void compararLetras() {
+
+       
+        if (!letrasUsada.contains(letra) && !estado) {
+            letrasUsada.add(letra);
+            char[] cadena = palabraElegida.toCharArray();
+            for (int i = 0; i < palabra.length(); i++) {
+                if (palabra.charAt(i) == letra) {
+                    cadena[i] = letra;
+                    setCoinciden(true);
+                    cont++;
+                }
+            }
+            if (isCoinciden() == false) {
+                intento--;
+                fallo++;
+
+            }
+            if (intento > 0 && cont <= palabra.length() - 1) {
+                coinciden = false;
+                palabraElegida = String.valueOf(cadena);
+            } else {
+                if (intento == 0 && fallo == palabraElegida.length()) {
+                    setResultado("GAME OVER");
+                } else {
+                    if (cont > palabra.length() - 1) {
+                        setResultado("GANASTE");
+                    }
+                }               
+                palabraElegida = palabra;
+                letrasUsada= new ArrayList<>();
+                estado = true;
+            }
+        }
+    }
+
+    /**
+     * @return the palabras
+     */
+    public String[] getPalabras() {
+        return palabras;
+    }
+
+    /**
+     * @param palabras the palabras to set
+     */
+    public void setPalabras(String[] palabras) {
+        this.palabras = palabras;
+    }
+
+    /**
+     * @return the palabra
+     */
+    public String getPalabra() {
+        return palabra;
+    }
+
+    /**
+     * @param palabra the palabra to set
+     */
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    /**
+     * @return the palabraElegida
+     */
+    public String getPalabraElegida() {
+        return palabraElegida;
+    }
+
+    /**
+     * @param palabraElegida the palabraElegida to set
+     */
+    public void setPalabraElegida(String palabraElegida) {
+        this.palabraElegida = palabraElegida;
+    }
+
+    /**
+     * @return the letra
+     */
+    public char getLetra() {
+        return letra;
+    }
+
+    /**
+     * @param letra the letra to set
+     */
+    public void setLetra(char letra) {
+        this.letra = letra;
+    }
+
+    /**
+     * @return the intento
+     */
+    public int getIntento() {
+        return intento;
+    }
+
+    /**
+     * @param intento the intento to set
+     */
+    public void setIntento(int intento) {
+        this.intento = intento;
+    }
+
+    /**
+     * @return the fallo
+     */
+    public int getFallo() {
+        return fallo;
+    }
+
+    /**
+     * @param fallo the fallo to set
+     */
+    public void setFallo(int fallo) {
+        this.fallo = fallo;
+    }
+
+    /**
+     * @return the letrasUsada
+     */
+    public ArrayList<Character> getLetrasUsada() {
+        return letrasUsada;
+    }
+
+    /**
+     * @param letrasUsada the letrasUsada to set
+     */
+    public void setLetrasUsada(ArrayList<Character> letrasUsada) {
+        this.letrasUsada = letrasUsada;
+    }
+
+    /**
+     * @return the coinciden
+     */
+    public boolean isCoinciden() {
+        return coinciden;
+    }
+
+    /**
+     * @param coinciden the coinciden to set
+     */
+    public void setCoinciden(boolean coinciden) {
+        this.coinciden = coinciden;
+    }
+
+    /**
+     * @return the resultado
+     */
+    public String getResultado() {
+        return resultado;
+    }
+
+    /**
+     * @param resultado the resultado to set
+     */
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+
+}
